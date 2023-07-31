730082ae7efeea5c55daf5eb1dea74b6d5a755f6
ramazanfb61 <ramazanfb61@gmail.com>
2023-07-31 16:31:33 +0300

    continue

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,36 +3,42 @@ import { ref, onMounted, watch } from "vue"
 
 const keyboard = ["e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i", "z", "c", "ç", "v", "b", "n", "m", "ö"]
 
-const word = ref([])
-const words = ref([])
+const word = ref(Array(5).fill(""))
+const words = ref(Array(6).fill(Array(5).fill('')))
 const placeHolder = ref(" ")
 const tryCounter = ref(0)
 
+const order = ref(0)
+
 onMounted(() => {
-  console.log(word.value,words);
+  console.log(word.value, words.value);
 })
 
 function deleteLetter() {
-  word.value.pop()
+  if (order.value > 0) {
+    order.value--
+  }
+  word.value.splice(order.value, 1, "")
+  console.log(word.value);
 }
 function addLetter(letter) {
-  if (word.value.length < 5) {
+  if (order.value < 5) {
+    word.value.splice(order.value++, 1, letter)
+    console.log("order value", order.value);
     console.log(letter);
-    word.value.push(letter)
     console.log(word.value);
   }
 }
 
 function enterWord() {
-  if (word.value.length === 5) {
-    words.value.push(word.value)
-    console.log("burası word", word.value);
-    word.value = [];
-    tryCounter.value++
-    console.log("burası words", words.value);
 
+  tryCounter.value++
+  order.value = 0;
+  words.value.splice(order.value++, 0, word.value)
+  console.log("burası word", word.value);
+  word.value = [];
+  console.log("burası words", words.value);
 
-  }
 }
 
 function checkLetter(a) {
@@ -47,46 +53,38 @@ function checkLetter(a) {
 <template>
   <main class="flex justify-center lg:mt-10 md:mt-8 mt-4 ">
     <div class="grid grid-rows-6 gap-y-2 ">
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div v-if="words[0]" v-for="a in words[0]" class="boardItem">{{ a }}</div>
-        <div v-else-if="word.length > 0" v-for="a in word" class="boardItem">{{ a }}</div>
-        <div v-else v-for="a in 5" class="boardItem">{{ placeHolder }}</div>
-      </h1>
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-      </h1>
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-      </h1>
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-      </h1>
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-      </h1>
-      <h1 class="grid grid-cols-5 gap-x-2">
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-        <div class="boardItem">1</div>
-      </h1>
+      <div v-for="(value, key, index) in words" class="grid grid-cols-5 gap-x-2">
+        <div v-if="words[index]" v-for="a in words[index]" class="boardItem">{{ a }}</div>
+        <div v-else-if="tryCounter === index" v-for="a in word" class="boardItem">{{ a }}</div>
+      </div>
+      <!-- <div class="grid grid-cols-5 gap-x-2"> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+      <!-- </div> -->
+      <!-- <div class="grid grid-cols-5 gap-x-2"> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+      <!-- </div> -->
+      <!-- <div class="grid grid-cols-5 gap-x-2"> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+      <!-- </div> -->
+      <!-- <div class="grid grid-cols-5 gap-x-2"> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+        <!-- <div class="boardItem">1</div> -->
+      <!-- </div> -->
     </div>
   </main>
 

1091ccb0087c14f5b7ac783011cc976a3f3b311f
ramazanfb61 <ramazanfb61@gmail.com>
2023-08-07 14:34:07 +0300

    start

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -3,8 +3,8 @@ import { ref, onMounted, watch } from "vue"
 
 const keyboard = ["e", "r", "t", "y", "u", "ı", "o", "p", "ğ", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ş", "i", "z", "c", "ç", "v", "b", "n", "m", "ö"]
 
-const word = ref(Array(5).fill(null))
-const words = ref(Array(6).fill(Array(5).fill('')))
+const word = ref(Array(5).fill({txt : '', exist : false}))
+const words = ref(Array(6).fill(Array(5).fill({txt : '', exist : false})))
 const tryCounter = ref(0)
 const wordStatus = ref([])
 const order = ref(0)
@@ -19,7 +19,7 @@ function deleteLetter() {
 }
 function addLetter(letter) {
   if (order.value < 5) {
-    word.value.splice(order.value++, 1, letter)
+    word.value.splice(order.value++, 1, {txt : letter,exist:false})
     console.log("order value", order.value);
     console.log(letter);
     console.log(word.value);
@@ -37,17 +37,16 @@ function enterWord() {
     words.value.splice(tryCounter.value++, 1, word.value)
     checkWord()
     console.log("tryCounter", tryCounter.value);
-    console.log("burası word", word.value);
-    word.value = Array(5).fill(null);
-    console.log("burası words", words.value);
+    console.log("word", word.value);
+    word.value = Array(5).fill({txt : '', exist : false});
+    console.log("words", words.value);
   }
 }
 function checkWord() {
-  wordStatus.value = []
   console.log(tryCounter.value);
-  wordStatus.value = words.value[tryCounter.value -1].filter((element, index) => answer.value.find((el => el === element)));
+  wordStatus.value.push(words.value[tryCounter.value -1].filter((element, index) => answer.value.find((el => el === element.txt))))
   console.log("wordStatus", wordStatus.value);
-  console.log("hello buras", words.value[tryCounter.value - 1]);
+  console.log(`words ${tryCounter.value -1} elemanı`, words.value[tryCounter.value - 1]);
 }
 </script>
 
@@ -55,10 +54,10 @@ function checkWord() {
   <main class="flex justify-center lg:mt-10 md:mt-8 mt-4 ">
     <div class="grid grid-rows-6 gap-y-2 font-semibold text-3xl">
       <div v-for="(item, index) in words" class="grid grid-cols-5 gap-x-2">
-        <div v-if="tryCounter === index" v-for="a in word" class="boardItem">{{ a }}</div>
+        <div v-if="tryCounter === index" v-for="a in word" class="boardItem">{{ a.txt }}</div>
         <div v-else-if="item" v-for="(a, index) in item" class="boardItem  ease-linear duration-300"
-          :class="a === answer[index] ? { 'bg-green-600': a === answer[index] } : { 'bg-yellow-700': wordStatus.includes(a) }">
-          {{ a }}
+          :class="a.txt === answer[index] ? { 'bg-green-600': a.txt === answer[index] } : { 'bg-yellow-700': wordStatus.includes(a.txt) }">
+          {{ a.txt }}
         </div>
       </div>
     </div>
